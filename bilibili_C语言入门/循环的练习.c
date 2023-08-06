e0d69889467ec615e432d7474882b7a26d2df8e3
JingyiWu-codespace <140898578+JingyiWu-codespace@users.noreply.github.com>
2023-08-06 19:53:41 +0800

    Update loop exercise .c

--- bilibili_C语言入门/循环的练习.c
+++ bilibili_C语言入门/循环的练习.c
@@ -140,6 +140,23 @@ int main ()
 //计算1/1-1/2+1/3-1/4+1/5-1/6.....+1/99-1/100
 
 这题蒙圈了
+# include <stdio.h>
+int main()
+{
+    int i = 0;
+    double sum = 0;
+    int flag = 1
+
+    for (i =1;i<=100'i++)
+        {
+        sum = sum + flag * 1.0/i;
+        flag = -flag;
+            
+        }
+    printf("%lf",sum);
+    return 0;
+}
+
 
 
 // 求十个整数中最大值
