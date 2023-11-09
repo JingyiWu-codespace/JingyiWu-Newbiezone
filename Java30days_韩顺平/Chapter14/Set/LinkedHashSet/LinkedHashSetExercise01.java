6bc280fb5863c704600546ba9822cffdfecbd904
JingyiWu-codespace <140898578+JingyiWu-codespace@users.noreply.github.com>
2023-11-09 23:51:00 +0100

    Add files via upload

---
+++ Java30days_韩顺平/Chapter14/Set/LinkedHashSet/LinkedHashSetExercise01.java
@@ -0,0 +1,64 @@
+package Java30days_韩顺平.Chapter14.Collection.Set.LinkedHashSet;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+
+public class LinkedHashSetExercise01 {
+    public static void main(String[] args) {
+        LinkedHashSet linkedHashSet = new LinkedHashSet();
+        linkedHashSet.add(new Car("奥拓",1000));
+        linkedHashSet.add(new Car("奥迪",300000));
+        linkedHashSet.add(new Car("法拉利",10000000));
+        linkedHashSet.add(new Car("奥迪",300000));
+        linkedHashSet.add(new Car("保时捷",7000000));
+        linkedHashSet.add(new Car("奥迪",300000));
+        for (Object o : linkedHashSet){
+            System.out.println(o);
+        }
+    }
+}
+class Car{
+    private String name;
+    private double price;
+    public Car(String name,double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Car car = (Car) object;
+        return Double.compare(price, car.price) == 0 && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+}
